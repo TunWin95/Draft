ead421d260799c630880c0a3f83d109176f206c1
TunWin95 <kartislin@gmail.com>
2020-10-14 23:31:11 +0630

    Initial Commit

---
+++ Draft/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Draft
+//
+//  Created by MAC on 14/10/2020.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
